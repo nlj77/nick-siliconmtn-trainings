5069ccded0e1b8b07f2578808ac2bcc5d37dce26
Nick Jones <nick.jones@siliconmtn.com>
2022-12-19 12:21:40 -0700

    Finished excercise 2 on intro to programming 1

---
+++ src/excercises/Excercise2.java
@@ -0,0 +1,34 @@
+package excercises;
+
+public class Excercise2 {
+	public static void zeroToTen() {
+		int x = 1;
+		while (x <= 10) {
+			System.out.println(x);
+			++x;
+		}
+	}
+	public static void tenToZero() {
+		for (int i = 10; i >= 0; --i) {
+			System.out.println(i);
+		}
+	}
+	public static void oneToTwentyEven() {
+		for (int i = 1; i <= 20; ++i) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+		}
+		}
+
+	}
+	
+	public static void main(String[] args) {
+		zeroToTen();
+		System.out.println("------------------");
+		tenToZero();
+		System.out.println("------------------");
+		oneToTwentyEven();
+		
+	
+	}
+}
--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author nickjones
- *
- */
-module nicksiliconmtntrainings {
-}
