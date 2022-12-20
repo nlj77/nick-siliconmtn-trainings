04a2c5a822be491c55999e7c2986344a02743610
Nick Jones <nick.jones@siliconmtn.com>
2022-12-20 07:55:46 -0700

    Updates exercise 2 with comments

--- src/excercises/Excercise2.java
+++ src/excercises/Excercise2.java
@@ -15,6 +15,10 @@ package excercises;
  * @updates:
  ****************************************************************************/
 public class Excercise2 {
+	/**
+	 * @params x this creates an instance of int and assigns it to 1, then while x is less than
+	 * or equal to 10, print out x to the console, then increment x by 1, then print it again.
+	 */
 	public static void zeroToTen() {
 		int x = 1;
 		while (x <= 10) {
@@ -22,13 +26,20 @@ public class Excercise2 {
 			++x;
 		}
 	}
-	
+	/**
+	 * @params i, this creates a for loop using i as a counter. Set i equal to 10, then while i is greater
+	 * or equal to 0, print i and decrement it by 1 each time. 
+	 */
 	public static void tenToZero() {
 		for (int i = 10; i >= 0; --i) {
 			System.out.println(i);
 		}
 	}
-	
+	/**
+	 * @params i, this declares another local variable i as an int, sets it equal to 1, and has the console print i
+	 * so long as i divided by 2, has a remainder of zero, using the modus operator. Then increment i by 1,
+	 * and check the criteria again. If i is divisible without a remainder of zero, do not print i.
+	 */
 	public static void oneToTwentyEven() {
 		for (int i = 1; i <= 20; ++i) {
 				if(i % 2 == 0) {
@@ -36,6 +47,9 @@ public class Excercise2 {
 				}
 		}
 	}
+	/** Calls the methods to print to console, and separates them with '--------'
+	 * 
+	 * */
 	
 	public static void main(String[] args) {
 		zeroToTen();
