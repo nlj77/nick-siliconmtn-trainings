6f648ef95eca7b3b858ad621a644c03ed9eac736
Nick Jones <nick.jones@siliconmtn.com>
2022-12-20 07:44:24 -0700

    Adds template for exercise

--- src/excercises/Excercise2.java
+++ src/excercises/Excercise2.java
@@ -1,5 +1,19 @@
 package excercises;
-
+/****************************************************************************
+ * <b>Title:</b> Example1.java
+ * <b>Project:</b> nicksiliconmtntrainings
+ * <b>Description:</b> Exercise 2, 
+ * This task, asks me to write methods to print the numbers 1 through 10, 10 descending to 1, and
+ * every even number from 2 to 20.
+ * This is the second exercise on the Intro to Programming slideshow
+ * <b>Copyright:</b> Copyright (c) 2022
+ * <b>Company:</b> Silicon Mountain Technologies
+ * 
+ * @author Nick Jones
+ * @version 3.0
+ * @since Dec 20th, 2022
+ * @updates:
+ ****************************************************************************/
 public class Excercise2 {
 	public static void zeroToTen() {
 		int x = 1;
