9381c5ade28ce6da106d395d8f3ae9be645e68f5
Nick Jones <nick.jones@siliconmtn.com>
2022-12-20 08:35:09 -0700

    Adds a stack class which demonstrates practice with class constructors and method overloading

---
+++ src/excercises/Stack.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package excercises;
+
+/****************************************************************************
+ * <b>Title:</b> Stack.java
+ * <b>Project:</b> nicksiliconmtntrainings
+ * <b>Description:</b> 
+ * <b>Copyright:</b> Copyright (c) 2022
+ * <b>Company:</b> Silicon Mountain Technologies
+ * 
+ * @author Nick Jones
+ * @version 3.0
+ * @since Dec 20 2022
+ * @updates:
+ ****************************************************************************/
+public class Stack {
+
+}
