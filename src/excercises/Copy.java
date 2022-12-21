ac3bc383dc56fc2838bc14f5735419f6ae0874e5
Nick Jones <nick.jones@siliconmtn.com>
2022-12-21 09:57:44 -0700

    Fixed indentations with Solar

---
+++ src/excercises/Copy.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package excercises;
+import java.io.*;
+/****************************************************************************
+ * <b>Title:</b> Copy.java
+ * <b>Project:</b> nicksiliconmtntrainings
+ * <b>Description:</b> Copy, 
+ * This shows off how a continued use of I/O Streams with the powerpoint Intro to Programming Class 3
+ * <b>Copyright:</b> Copyright (c) 2022
+ * <b>Company:</b> Silicon Mountain Technologies
+ * 
+ * @author Nick Jones
+ * @version 3.0
+ * @since Dec 21 2022
+ * @updates:
+ ****************************************************************************/
+public class Copy {
+	
+}
